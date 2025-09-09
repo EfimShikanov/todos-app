7846e4b7b70b855b3c515f3556b7d7b8c785dc6c
Efim Shikanov <shikanovefim@gmail.com>
2025-09-10 00:25:33 +0500

    refactor: move manifest to app folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-'use client';
+// 'use client';
 
 import { Roboto } from 'next/font/google';
 import './globals.css';
@@ -38,7 +38,7 @@ export default function RootLayout({
         <link rel="icon" href="/icons/96x96.png" />
         <link rel="icon" href="/icons/72x72.png" />
         <link rel="icon" href="/icons/48x48.png" />
-        <link rel="manifest" href="/manifest.json" />
+        {/*<link rel="manifest" href="/manifest.json" />*/}
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <body className={`${roboto.variable}`}>
